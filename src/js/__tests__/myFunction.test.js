de531f6fe0593095b6adfd676dae1158e0ea3424
alexgithub <alexgithub@yandex.ru>
2024-02-17 10:51:24 +1000

    2024.02.17 Удалил коминтарии

--- src/js/__tests__/myFunction.test.js
+++ src/js/__tests__/myFunction.test.js
@@ -1,6 +1,19 @@
 import healthColor from '../myFunction';
 
 test('checking myFunction, healthy', () => {
-    const ivan = {who: 'Маг', health: 90};
-    expect(healthColor(ivan)).toBe('healthy');
+    const Oleg = {who: 'Маг', health: 90};
+    const result = healthColor(Oleg);
+    expect(result).toBe('healthy');
+});
+
+test('checking myFunction, wounded', () => {
+    const Oleg = {who: 'Маг', health: 45};
+    const result = healthColor(Oleg);
+    expect(result).toBe('wounded');
+});
+
+test('checking myFunction, critical', () => {
+    const Oleg = {who: 'Маг', health: 5};
+    const result = healthColor(Oleg);
+    expect(result).toBe('critical');
 });
