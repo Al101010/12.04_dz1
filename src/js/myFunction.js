ec681fbb273834c2bec1c1e76e9eff66a266e67f
alexgithub <alexgithub@yandex.ru>
2024-02-17 11:22:17 +1000

    2024.02.17 Удалил ;

--- src/js/myFunction.js
+++ src/js/myFunction.js
@@ -1,12 +1,12 @@
 export default function healthColor(user) {
     if (user.health < 15){
         return 'critical';
-    };
+    }
     if  (user.health >= 15 && user.health <= 50 ){
         return 'wounded';
-    };
+    }
     if  (user.health > 50 ){
         return 'healthy';
-    };
+    }
 }
 
